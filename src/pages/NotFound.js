ad7c6ab7454045ed8993681b48aee95bb9593db4
vanshshah55 <vips.vansh@gmail.com>
2025-01-29 13:20:05 +0530

    thirteenth commit/errorpage

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,25 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./NotFound.css"; // Import CSS for futuristic effects
-import { HashLink } from 'react-router-hash-link';
+
 
 const NotFound = () => {
   return (
     <div className="not-found-container">
       <div className="glitch" data-text="404">404</div>
-      <h2 className="title">Lost in Cyberspace?</h2>
+      <h2 className="title">ERROR!!!!</h2>
       <p className="description">
-        It looks like you’ve entered an unknown dimension.  
-        But don’t worry, we’ll teleport you back! 🚀
+        It looks like you’ve lost your way.  
+        But don’t worry, Get back to exploring Vipsee Infotech! 🚀
       </p>
 
       <div className="btn-container">
         <Link to="/" className="btn neon">
           🏠 Return to Home
         </Link>
-        <HashLink to="/#services" className="btn neon secondary">
-          🔍 Explore Services
-        </HashLink>
+        
       </div>
     </div>
   );
